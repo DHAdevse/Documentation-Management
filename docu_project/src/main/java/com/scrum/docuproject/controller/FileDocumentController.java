78a6fcd4de4d85615fa2b0ec60b9f37d7ba3f4ab
Dinh Huu Quang Anh <125935864+quanganh247-qa@users.noreply.github.com>
2024-03-20 09:59:42 +0700

    add fire base controller

--- docu_project/src/main/java/com/scrum/docuproject/controller/FileDocumentController.java
+++ docu_project/src/main/java/com/scrum/docuproject/controller/FileDocumentController.java
@@ -1,20 +1,36 @@
 package com.scrum.docuproject.controller;
 
 import com.scrum.docuproject.models.FileDocument;
+import com.scrum.docuproject.models.Versions;
+import com.scrum.docuproject.repository.DocumentRepository;
+import com.scrum.docuproject.repository.UserRepository;
 import com.scrum.docuproject.service.DocumentService;
-import com.scrum.docuproject.service.DocumentServiceImpl;
+import com.scrum.docuproject.service.FirebaseService;
+import com.scrum.docuproject.service.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.parameters.P;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/main")
+@RequestMapping("/document")
 public class FileDocumentController {
+    @Autowired
     private DocumentService documentService;
+
+    @Autowired
+    private FirebaseService uploadService;
+    @Autowired
+    private DocumentRepository documentRepository;
+    @Autowired
+    UserRepository userRepository;
 
     @Autowired
     public FileDocumentController(DocumentService documentService) {
@@ -24,7 +40,10 @@ public class FileDocumentController {
     @PostMapping("/create")
     public ResponseEntity<FileDocument> createDocument(@RequestBody FileDocument fileDocument)
     {
-        documentService.addDocument(fileDocument);
+        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        fileDocument.setUser(userRepository.findUserById(userDetails.getId()));
+        fileDocument.setVersions(new ArrayList<>());
+        documentRepository.save(fileDocument);
         return ResponseEntity.ok().body(fileDocument);
     }
 
@@ -56,4 +75,31 @@ public class FileDocumentController {
         }
         return ResponseEntity.notFound().build();
     }
+
+    @PostMapping("/{id}/version/add-file")
+    public ResponseEntity<Versions> addFile(@PathVariable String id, @RequestParam("file") MultipartFile file) throws Exception{
+        Optional<FileDocument> fileDocument = documentRepository.findById(id);
+        Versions versions = new Versions();
+        List<Versions> versionsList = fileDocument.get().getVersions();
+
+//        String extension = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf('.'));
+        String extension = file.getOriginalFilename();
+        versions.setNameVer(extension);
+        versions.setId(String.valueOf(versionsList.size() + 1));
+        versions.setLink(uploadService.uploadFile(file));
+        LocalDateTime time = LocalDateTime.now();
+        String formatTime = time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        versions.setDate(formatTime);
+        versions.setMessage("");
+        if(versionsList.isEmpty()){
+            versionsList = new ArrayList<>();
+        }
+        versionsList.add(versions);
+        fileDocument.get().setVersions(versionsList);
+        documentRepository.save(fileDocument.get());
+        return ResponseEntity.ok(versions);
+
+    }
+
+
 }
